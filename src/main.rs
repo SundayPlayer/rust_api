d02d06f0f158e753db1f59521254d9db6edf68dc
SundayPlayer <baptiste.vade@sundayplayer.fr>
2019-01-11 17:05:09 +0100

    Use Result to validate segment and decide what to do

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,17 @@ fn hello_dynamic(name: &RawStr) -> String {
     format!("Hello, {}", name.as_str())
 }
 
+#[get("/new/<id>")]
+fn new_article (id: Result<usize, &RawStr>) -> String {
+    match id {
+        Ok(uid) => format!("It's a 'real' id, let's do something with it. (Id = {})", uid),
+        Err(crap) => format!("Either it's a isize or it's a crap (Crap = {})", crap)
+    }
+}
+
 fn main() {
     rocket::ignite()
     .mount("/", routes![index, post_index, hello_dynamic])
+    .mount("/article", routes![new_article])
     .launch();
 }
